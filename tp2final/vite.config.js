94cfa40a48f92adeed8b58ac501a46e752023418
MatiasCavilla01 <Matias.cavilla20038@outlook.com>
2024-07-31 05:43:09 -0300

    configuracion para netlify

--- tp2final/vite.config.js
+++ tp2final/vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.svg','**/*.PNG'],
-})
+  build: {
+    rollupOptions: {
+      input: './index.html', // Asegúrate de que este archivo esté presente en la carpeta raíz
+    },
+  },
+  assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.svg', '**/*.PNG'],
+});
