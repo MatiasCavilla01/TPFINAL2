ebc99982578298421e2290519fd6bfbcf6c3cc20
MatiasCavilla01 <Matias.cavilla20038@outlook.com>
2024-07-31 06:10:36 -0300

    configuracion para netlify

--- tp2final/vite.config.js
+++ tp2final/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  root:'.tp2final',
   plugins: [react()],
   build: {
     rollupOptions: {
